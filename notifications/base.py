83624b581ca1bee31d3a2d64b0820f69618b4ae6
malavdnm <38399975+malavdnm@users.noreply.github.com>
2025-01-09 16:30:36 +0530

    Update base.py

--- notifications/base.py
+++ notifications/base.py
@@ -5,12 +5,3 @@ class NotificationStrategy(ABC):
     @abstractmethod
     async def notify(self, message: str):
         pass
-
-
-# notifications/console.py
-from notifications.base import NotificationStrategy
-
-
-class ConsoleNotification(NotificationStrategy):
-    async def notify(self, message: str):
-        print(f"Scraping notification: {message}")
